339438bf196f907ec496e3d1d7b14586f5d6d92a
Nela Jović <nelanekijovic@gmail.com>
2022-12-24 13:00:59 +0100

    Klasa Alarm

---
+++ PubSubEngine/Models/Alarm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [DataContract]
+    public enum AlarmMessages
+    {
+        [EnumMember]
+        Low,
+        [EnumMember]
+        Standard,
+        [EnumMember]
+        High
+
+    }
+    [DataContract]
+    [Serializable]
+    public class Alarm
+    {
+        [DataMember]
+        private DateTime GeneratingTime;
+        [DataMember]
+        private string MessegAlarm;
+        [DataMember]
+        private int risk;
+
+        public int Risk
+        {
+            get
+            {
+                return risk;
+            }
+            set
+            {
+                if(value <= 100 && value>=1)
+                    risk = value;
+            }
+        }
+    }
+}
--- PubSubEngine/Models/Properties/AssemblyInfo.cs
+++ PubSubEngine/Models/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HP Inc.")]
 [assembly: AssemblyProduct("Models")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba09db54-30bc-494b-af6f-bd43036b0a8e")]
+[assembly: Guid("bf60cdb5-0245-4808-946f-d4ff2ae9efd8")]
 
 // Version information for an assembly consists of the following four values:
 //
